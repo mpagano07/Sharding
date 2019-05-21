30659739d00346c3fde5b3f56642fa692dca6b00
Matias Pagano <41879726+mpagano07@users.noreply.github.com>
2019-05-21 10:09:09 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -5,3 +5,23 @@ require_once 'Cluster.php';
 require_once 'Cola.php';
 require_once 'Persona.php';
 
+$cluster = new Cluster(new Cola());
+$cluster->agregarDB(new DB());
+// $cluster->migrar();
+$cluster->agregarDB(new DB());
+// $cluster->migrar();
+$cluster->agregarDB(new DB());
+// $cluster->migrar();
+
+$cluster->guardar(new Persona("Pepe", 32));
+$cluster->guardar(new Persona("Matias", 10));
+$cluster->guardar(new Persona("Julian", 9));
+$cluster->guardar(new Persona("Jose", 44));
+$cluster->guardar(new Persona("Adrian", 55));
+$cluster->guardar(new Persona("KP", 60));
+$cluster->guardar(new Persona("Tomy", 70));
+
+$cluster->agregarDB(new DB());
+$cluster->migrar();
+
+$cluster->mostarResumen();
